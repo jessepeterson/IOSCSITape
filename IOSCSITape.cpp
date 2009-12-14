d933e71ec962f1d2570b2555fac7f00d1ff1e223
Jesse Peterson <jesse.c.peterson@gmail.com>
2009-12-13 17:39:38 -0800

    Added SSC CDB builders.

--- IOSCSITape.cpp
+++ IOSCSITape.cpp
@@ -1,3 +1,7 @@
+#include <AssertMacros.h>
+
+#include <IOKit/scsi/SCSICommandOperationCodes.h>
+
 #include "IOSCSITape.h"
 
 #define super IOSCSIPrimaryCommandsDevice
@@ -66,4 +70,378 @@ bool
 IOSCSITape::ClearNotReadyStatus(void)
 {
 	return false;
+}
+
+#if 0
+#pragma mark -
+#pragma mark 0x01 SSC Implicit Address Commands
+#pragma mark -
+#endif /* 0 */
+
+bool
+IOSCSITape::ERASE_6(
+	SCSITaskIdentifier	request,
+	SCSICmdField1Bit	IMMED,
+	SCSICmdField1Bit	LONG,
+	SCSICmdField1Byte	CONTROL)
+{
+	bool result = false;
+	
+	require(IsParameterValid(IMMED, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(LONG, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_ERASE, 
+							  (LONG << 1) |
+							   IMMED, 
+							  0x00, 
+							  0x00, 
+							  0x00, 
+							  CONTROL);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_NoDataTransfer);
+	
+ErrorExit:
+	
+	return result;
+}				   
+
+bool
+IOSCSITape::READ_6(
+	SCSITaskIdentifier		request,
+	IOMemoryDescriptor *		readBuffer,
+	UInt32					blockSize,
+	SCSICmdField1Bit			SILI,
+	SCSICmdField1Bit			FIXED,
+	SCSICmdField3Byte		TRANSFER_LENGTH,
+	SCSICmdField1Byte		CONTROL)
+{
+	UInt32	requestedByteCount	= 0;
+	bool	result				= false;
+	
+	require(IsParameterValid(SILI, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(FIXED, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(TRANSFER_LENGTH, kSCSICmdFieldMask3Byte), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	if (FIXED)
+		requestedByteCount = TRANSFER_LENGTH * blockSize;
+	else
+		requestedByteCount = TRANSFER_LENGTH;
+	
+	require((readBuffer != 0), ErrorExit);
+	require((readBuffer->getLength() >= requestedByteCount), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_READ_6, 
+							  (SILI << 1) |
+							   FIXED, 
+							  (TRANSFER_LENGTH >> 16) & 0xFF, 
+							  (TRANSFER_LENGTH >>  8) & 0xFF, 
+							   TRANSFER_LENGTH        & 0xFF, 
+							  CONTROL);
+	
+	SetDataBuffer(request, readBuffer);
+	
+	SetRequestedDataTransferCount(request, requestedByteCount);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_FromTargetToInitiator);
+	
+	SetTimeoutDuration(request, kThirtySecondTimeoutInMS);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+bool
+IOSCSITape::SPACE_6(
+	SCSITaskIdentifier	request,
+	SCSICmdField4Bit	CODE,
+	SCSICmdField3Byte	COUNT,
+	SCSICmdField1Byte	CONTROL)
+{
+	bool result = false;
+	
+	require(IsParameterValid(CODE, kSCSICmdFieldMask4Bit), ErrorExit);
+	
+	/* allow a two's complement negative number in the 3-byte integer
+	 * space to not cause an error with IsParameterValid() */
+	if ((COUNT & 0xFF800000) == 0xFF800000)
+		COUNT = COUNT & 0xFFFFFF;
+	
+	require(IsParameterValid(COUNT, kSCSICmdFieldMask3Byte), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_SPACE, 
+							  CODE, 
+							  (COUNT >> 16) & 0xFF, 
+							  (COUNT >>  8) & 0xFF, 
+							   COUNT        & 0xFF, 
+							  CONTROL);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_NoDataTransfer);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+bool
+IOSCSITape::WRITE_6(
+	SCSITaskIdentifier		request,
+	IOMemoryDescriptor *	writeBuffer,
+	UInt32					blockSize,
+	SCSICmdField1Bit		FIXED,
+	SCSICmdField3Byte		TRANSFER_LENGTH,
+	SCSICmdField1Byte		CONTROL)
+{
+	UInt32	requestedByteCount	= 0;
+	bool	result				= false;
+	
+	require(IsParameterValid(FIXED, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(TRANSFER_LENGTH, kSCSICmdFieldMask3Byte), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	if (FIXED)
+		requestedByteCount = TRANSFER_LENGTH * blockSize;
+	else
+		requestedByteCount = TRANSFER_LENGTH;
+	
+	require((writeBuffer != 0), ErrorExit);
+	require((writeBuffer->getLength() >= requestedByteCount), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_WRITE_6, 
+							  FIXED, 
+							  (TRANSFER_LENGTH >> 16) & 0xFF, 
+							  (TRANSFER_LENGTH >>  8) & 0xFF, 
+							   TRANSFER_LENGTH        & 0xFF, 
+							  CONTROL);
+	
+	SetDataBuffer(request, writeBuffer);
+	
+	SetRequestedDataTransferCount(request, requestedByteCount);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_FromInitiatorToTarget);
+	
+	SetTimeoutDuration(request, kThirtySecondTimeoutInMS);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+bool
+IOSCSITape::WRITE_FILEMARKS_6(
+	SCSITaskIdentifier	request,
+	SCSICmdField1Bit	WSMK,
+	SCSICmdField1Bit	IMMED,
+	SCSICmdField3Byte	TRANSFER_LENGTH,
+	SCSICmdField1Byte	CONTROL)
+{
+	bool result = false;
+	
+	require(IsParameterValid(WSMK, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(IMMED, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(TRANSFER_LENGTH, kSCSICmdFieldMask3Byte), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_WRITE_FILEMARKS, 
+							  (WSMK << 1) |
+							   IMMED, 
+							  (TRANSFER_LENGTH >> 16) & 0xFF, 
+							  (TRANSFER_LENGTH >>  8) & 0xFF, 
+							   TRANSFER_LENGTH        & 0xFF, 
+							  CONTROL);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_NoDataTransfer);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+#if 0
+#pragma mark -
+#pragma mark 0x01 SSC Common Commands
+#pragma mark -
+#endif /* 0 */
+
+bool
+IOSCSITape::LOAD_UNLOAD(
+	SCSITaskIdentifier	request,
+	SCSICmdField1Bit	IMMED,
+	SCSICmdField1Bit	HOLD,
+	SCSICmdField1Bit	EOT,
+	SCSICmdField1Bit	RETEN,
+	SCSICmdField1Bit	LOAD,
+	SCSICmdField1Byte	CONTROL)
+{
+	bool result = false;
+	
+	require(IsParameterValid(IMMED, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(HOLD, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(EOT, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(RETEN, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(LOAD, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_LOAD_UNLOAD, 
+							  IMMED, 
+							  0x00, 
+							  0x00, 
+							  (HOLD  << 3) |
+							  (EOT   << 2) |
+							  (RETEN << 1) |
+							   LOAD, 
+							  CONTROL);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_NoDataTransfer);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+bool
+IOSCSITape::READ_BLOCK_LIMITS(
+	SCSITaskIdentifier		request,
+	IOMemoryDescriptor *	readBuffer,
+	SCSICmdField1Byte		CONTROL)
+{
+	bool result = false;
+	
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	require((readBuffer != 0), ErrorExit);
+	require((readBuffer->getLength() >= 6), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_READ_BLOCK_LIMITS, 
+							  0x00, 
+							  0x00, 
+							  0x00, 
+							  0x00, 
+							  CONTROL);
+	
+	SetDataBuffer(request, readBuffer);
+	
+	SetRequestedDataTransferCount(request, 6);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_FromTargetToInitiator);
+	
+	SetTimeoutDuration(request, kThirtySecondTimeoutInMS);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+bool
+IOSCSITape::READ_POSITION(
+	SCSITaskIdentifier		request,
+	IOMemoryDescriptor *	buffer,
+	SCSICmdField5Bit		SERVICE_ACTION,
+	SCSICmdField2Byte		ALLOCATION_LENGTH,
+	SCSICmdField1Byte		CONTROL)
+{
+	bool	result	= false;
+	int		count	= 0;
+	
+	require(IsParameterValid(SERVICE_ACTION, kSCSICmdFieldMask5Bit), ErrorExit);
+	require(IsParameterValid(ALLOCATION_LENGTH, kSCSICmdFieldMask2Byte), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	require((buffer != 0), ErrorExit);
+	
+	switch (SERVICE_ACTION)
+	{
+		case kSCSIReadPositionServiceAction_ShortFormBlockID:
+		case kSCSIReadPositionServiceAction_ShortFormVendorSpecific:
+			count = 20;
+			break;
+		case kSCSIReadPositionServiceAction_LongForm:
+			count = 32;
+			break;
+		case kSCSIReadPositionServiceAction_ExtendedForm:
+			count = 28;
+			break;
+		default:
+			/* unknown service action */
+			count = buffer->getLength();
+			break;
+	}
+	
+	require((buffer->getLength() >= count), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_READ_POSITION, 
+							  SERVICE_ACTION, 
+							  0x00, 
+							  0x00, 
+							  0x00, 
+							  0x00,
+							  0x00,
+							  (ALLOCATION_LENGTH >> 8) & 0xFF,
+							   ALLOCATION_LENGTH       & 0xFF,
+							  CONTROL);
+	
+	SetDataBuffer(request, buffer);
+	
+	SetRequestedDataTransferCount(request, count);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_FromTargetToInitiator);
+	
+	SetTimeoutDuration(request, kThirtySecondTimeoutInMS);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
+}
+
+bool
+IOSCSITape::REWIND(
+	SCSITaskIdentifier	request,
+	SCSICmdField1Bit	IMMED,
+	SCSICmdField1Byte	CONTROL)
+{
+	bool result = false;
+	
+	require(IsParameterValid(IMMED, kSCSICmdFieldMask1Bit), ErrorExit);
+	require(IsParameterValid(CONTROL, kSCSICmdFieldMask1Byte), ErrorExit);
+	
+	SetCommandDescriptorBlock(request, 
+							  kSCSICmd_REWIND, 
+							  IMMED, 
+							  0x00, 
+							  0x00, 
+							  0x00, 
+							  CONTROL);
+	
+	SetDataTransferDirection(request, kSCSIDataTransfer_NoDataTransfer);
+	
+	result = true;
+	
+ErrorExit:
+	
+	return result;
 }
--- IOSCSITape.h
+++ IOSCSITape.h
@@ -1,5 +1,39 @@
 #include <IOKit/scsi/IOSCSIMultimediaCommandsDevice.h>
 
+/* These were defined in the OS-supplied SCSICommandOperationCodes.h but
+ * "#if 0"-ed out. May need to back these out if the official ones ever
+ * get uncommented. */
+/* Commands defined by the T10:997-D SCSI-3 Stream Commands (SSC) command
+ * specification.  The definitions and section numbers are based on section 5
+ * of the revision 22, January 1, 2000 version of the specification. 
+ */
+enum
+{
+    kSCSICmd_ERASE                          = 0x19, /* Sec. 5.3.1: Mandatory */
+    kSCSICmd_FORMAT_MEDIUM                  = 0x04, /* Sec. 5.3.2: Optional */
+    kSCSICmd_LOAD_UNLOAD                    = 0x1B, /* Sec. 5.3.3: Optional */
+    kSCSICmd_LOCATE                         = 0x2B, /* Sec. 5.3.4: Optional */
+    kSCSICmd_MOVE_MEDIUM                    = 0xA5, /* SMC: Optional */
+    kSCSICmd_READ_BLOCK_LIMITS              = 0x05, /* Sec. 5.3.6: Mandatory */
+    kSCSICmd_READ_ELEMENT_STATUS            = 0xB8, /* SMC: Optional */
+    kSCSICmd_READ_POSITION                  = 0x34, /* Sec. 5.3.7: Mandatory */
+    kSCSICmd_READ_REVERSE                   = 0x0F, /* Sec. 5.3.8: Optional */
+    kSCSICmd_RECOVER_BUFFERED_DATA          = 0x14, /* Sec. 5.3.9: Optional */
+    kSCSICmd_REPORT_DENSITY_SUPPORT         = 0x44, /* Sec. 5.3.10: Mandatory*/
+    kSCSICmd_REWIND                         = 0x01, /* Sec. 5.3.11: Mandatory*/
+    kSCSICmd_SPACE                          = 0x11, /* Sec. 5.3.12: Mandatory*/
+    kSCSICmd_VERIFY_6                       = 0x13, /* Sec. 5.3.13: Optional */
+    kSCSICmd_WRITE_FILEMARKS                = 0x10  /* Sec. 5.3.15: Mandatory*/
+};
+
+enum
+{
+	kSCSIReadPositionServiceAction_ShortFormBlockID			= 0x00,
+	kSCSIReadPositionServiceAction_ShortFormVendorSpecific	= 0x01,
+	kSCSIReadPositionServiceAction_LongForm					= 0x06,
+	kSCSIReadPositionServiceAction_ExtendedForm				= 0x08
+};
+
 class IOSCSITape : public IOSCSIPrimaryCommandsDevice {
 	OSDeclareDefaultStructors(IOSCSITape)
 private:
@@ -18,4 +52,68 @@ private:
 
 	UInt32 GetNumberOfPowerStateTransitions(void);
 	bool ClearNotReadyStatus(void);
+	
+	/* SSC Implicit Address Commands */
+	bool ERASE_6(
+		SCSITaskIdentifier,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField1Byte);
+	
+	bool READ_6(
+		SCSITaskIdentifier,
+		IOMemoryDescriptor *,
+		UInt32,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField3Byte,
+		SCSICmdField1Byte);
+	
+	bool SPACE_6(
+		SCSITaskIdentifier,
+		SCSICmdField4Bit,
+		SCSICmdField3Byte,
+		SCSICmdField1Byte);
+	
+	bool WRITE_6(
+		SCSITaskIdentifier,
+		IOMemoryDescriptor *,
+		UInt32,
+		SCSICmdField1Bit,
+		SCSICmdField3Byte,
+		SCSICmdField1Byte);
+	
+	bool WRITE_FILEMARKS_6(
+		SCSITaskIdentifier,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField3Byte,
+		SCSICmdField1Byte);
+	
+	/* SSC Common Commands */
+	bool LOAD_UNLOAD(
+		SCSITaskIdentifier,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField1Bit,
+		SCSICmdField1Byte);
+	
+	bool READ_BLOCK_LIMITS(
+		SCSITaskIdentifier,
+		IOMemoryDescriptor *,
+		SCSICmdField1Byte);
+	
+	bool READ_POSITION(
+		SCSITaskIdentifier,
+		IOMemoryDescriptor *,
+		SCSICmdField5Bit,
+		SCSICmdField2Byte,
+		SCSICmdField1Byte);
+	
+	bool REWIND(
+		SCSITaskIdentifier,
+		SCSICmdField1Bit,
+		SCSICmdField1Byte);
 };

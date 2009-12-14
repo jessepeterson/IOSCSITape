a6dcb750e35d83e8394acb831823555b3b15f57b
Jesse Peterson <jesse.c.peterson@gmail.com>
2009-12-13 16:47:54 -0800

    Base class of IOSCSIPrimaryCommandsDevice. Implement pure methods as well as IOKit matching personalities in Info.plist.

--- IOSCSITape.cpp
+++ IOSCSITape.cpp
@@ -1,1 +1,69 @@
-/* add your code here */
+#include "IOSCSITape.h"
+
+#define super IOSCSIPrimaryCommandsDevice
+OSDefineMetaClassAndStructors(IOSCSITape, IOSCSIPrimaryCommandsDevice)
+
+UInt32
+IOSCSITape::GetInitialPowerState(void)
+{
+	return 0;
+}
+
+void
+IOSCSITape::HandlePowerChange(void)
+{
+}
+
+void
+IOSCSITape::HandleCheckPowerState(void)
+{
+}
+
+void
+IOSCSITape::TicklePowerManager(void)
+{
+}
+
+bool
+IOSCSITape::InitializeDeviceSupport(void)
+{
+	/* always initialize */
+	return true;
+}
+
+void
+IOSCSITape::StartDeviceSupport(void)
+{
+}
+
+void
+IOSCSITape::SuspendDeviceSupport(void)
+{
+}
+
+void
+IOSCSITape::ResumeDeviceSupport(void)
+{
+}
+
+void
+IOSCSITape::StopDeviceSupport(void)
+{
+}
+
+void
+IOSCSITape::TerminateDeviceSupport(void)
+{
+}
+
+UInt32
+IOSCSITape::GetNumberOfPowerStateTransitions(void)
+{
+	return 0;
+}
+
+bool
+IOSCSITape::ClearNotReadyStatus(void)
+{
+	return false;
+}
--- IOSCSITape.h
+++ IOSCSITape.h
@@ -1,1 +1,21 @@
-/* add your code here */
+#include <IOKit/scsi/IOSCSIMultimediaCommandsDevice.h>
+
+class IOSCSITape : public IOSCSIPrimaryCommandsDevice {
+	OSDeclareDefaultStructors(IOSCSITape)
+private:
+	/* pure function overrides from IOSCSIPrimaryCommandsDevice */
+	UInt32 GetInitialPowerState(void);
+	void HandlePowerChange(void);
+	void HandleCheckPowerState(void);
+	void TicklePowerManager(void);
+	
+	bool InitializeDeviceSupport(void);
+	void StartDeviceSupport(void);
+	void SuspendDeviceSupport(void);
+	void ResumeDeviceSupport(void);
+	void StopDeviceSupport(void);
+	void TerminateDeviceSupport(void);
+
+	UInt32 GetNumberOfPowerStateTransitions(void);
+	bool ClearNotReadyStatus(void);
+};
